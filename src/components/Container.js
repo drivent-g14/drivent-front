baeb89c5b9196101d62fc5f504660bc6443473db
carlos_casanova <carlosctu94@gmail.com>
2022-12-17 14:15:56 -0300

    feat: adding props on Container component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,15 +1,36 @@
 import styled from 'styled-components';
 
-export default styled.div`
-  height: 100vh;
-  max-height: ${props => props.height || '600px'};
+//[height]: Define a altura do container;
+//[width]: Define a largura do container;
+//[hasBoxShadow] - boolean: Deixa o componente com a cor default, ele vem como default true;
+// com um efeito de box-shadow;
+//[borderColor]: hasBoxShadow deve estar false para funcionar. Define a cor da borda;
+//[backgroundColor]: Define o background do container;
 
+export default function Container({ height, width, hasBoxShadow, borderColor, backgroundColor }) {
+  return (
+    <BoxContainer
+      height={height}
+      width={width}
+      hasBoxShadow={hasBoxShadow ?? true}
+      borderColor={borderColor ?? 'rgba(0, 0, 0, 0.2)'}
+      backgroundColor={backgroundColor}
+    />
+  );
+}
+
+const BoxContainer = styled.div`
+  height: 100vh;
+  max-height: ${(props) => props.height || '600px'};
   width: 100%;
-  max-width: ${props => props.width || '1200px'};
+  max-width: ${(props) => props.width || '1200px'};
 
   border-radius: 20px;
-  background-color: #fff;
-  box-shadow: 4px 4px 10px 5px rgba(0, 0, 0, .2);
+  background-color: ${(props) => props.backgroundColor || '#fff'};
+  border: ${(props) => (props.hasBoxShadow ? '' : '1px')};
+  border-style: ${(props) => (props.hasBoxShadow ? '' : 'solid')};
+  border-color: ${(props) => (props.hasBoxShadow ? '' : props.borderColor)};
+  box-shadow: ${(props) => (props.hasBoxShadow ? '4px 4px 10px 5px rgba(0, 0, 0, 0.2)' : '')};
 
   display: flex;
   overflow: hidden;
--- src/pages/Dashboard/Payment/index.js
+++ src/pages/Dashboard/Payment/index.js
@@ -1,3 +1,21 @@
+import styled from 'styled-components';
+import Container from '../../../components/Container';
+
 export default function Payment() {
-  return 'Pagamento: Em breve!';
+  return (
+    <>
+      <Divizinha>
+        <Container height="150px" width="150px" hasBoxShadow={false} borderColor="red" backgroundColor="blue" />
+        <Container height="150px" width="150px" />
+        <Container height="150px" width="150px" borderColor="blue" backgroundColor={'red'} />
+      </Divizinha>
+    </>
+  );
 }
+
+const Divizinha = styled.div`
+  display: flex;
+  row-gap: 45px;
+  column-gap: 45px;
+  flex-direction: row;
+`;
