d4061f4907008d8639cff5b47f3f3848f49cfea8
Aleisson <caioaleisson@gmail.com>
2022-12-19 19:59:21 -0300

    docs: add htmls configurations in page hotels

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -4,6 +4,12 @@ export default function Hotel() {
   return (
     <HotelsSections>
       <TitleSection>Escolha de Hotel e quarto</TitleSection>
+      <HotelsOptionSection>
+        <p>Primeiro, escolha seu hotel</p>
+        <HotelsCardsSection>
+       
+        </HotelsCardsSection>
+      </HotelsOptionSection>
     </HotelsSections>
   );
 }
@@ -11,9 +17,23 @@ const HotelsSections = styled.div`
   display: flex;
   flex-direction: column;
   row-gap: 32px;
-  background-color: red;
 `;
+
 const TitleSection = styled.p`
   font-size: 28px;
 `;
 
+const HotelsOptionSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap:16px;
+  font-size: 18px;
+  color: #9e9e9e;
+`;
+
+const HotelsCardsSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  column-gap:24px;
+  align-items: center;
+`;
