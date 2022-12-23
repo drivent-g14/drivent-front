04fb7bc7817ead0adc0280739505f9595ccdea46
carlos_casanova <carlosctu94@gmail.com>
2022-12-23 16:40:10 -0300

    feat: adding hotel unauthorized messages

--- src/pages/Dashboard/Hotel/index.js
+++ src/pages/Dashboard/Hotel/index.js
@@ -1,24 +1,68 @@
 import styled from 'styled-components';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import CardContainer from '../../../components/Dashboard/Containers/CardContainer';
 import DisplaySection from '../../../components/Dashboard/Sections/DisplaySection';
 import useHotel from '../../../hooks/api/useHotels';
+import { useGetTicket } from '../../../hooks/api/useTicket';
+import UnauthorizedScreen from '../../../components/Dashboard/Errors/UnauthorizedScreen';
+
+const unauthorizedMsg = Object.freeze({
+  NO_PAYMENT_MSG: 'Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem.',
+  ONLINE_MODALITY_MSG: 'Sua modalidade de ingresso não possui hospedagem. Prossiga para a escolha de atividades.',
+});
+
+const modalityTicket = Object.freeze({
+  ONLINE_MODALITY: 'ONLINE',
+  AT_EVENT_MODALITY: 'AT EVENT',
+});
+
+//TODO: move to components and refactor screen
+
 export default function Hotel() {
   const { hotels } = useHotel();
+  const { ticket } = useGetTicket();
+  const [unauthorizedMessage, setUnauthorizedMessage] = useState(unauthorizedMsg.NO_PAYMENT_MSG);
+  const [paidTicket, setPaidTicket] = useState(false);
+  const [ticketType, setTicketType] = useState(null);
 
-  useEffect(() => {}, [hotels]);
+  useEffect(() => {
+    if (ticket && ticket.status === 'PAID') {
+      setPaidTicket(true);
+      setTicketType(modalityTicket.AT_EVENT_MODALITY);
+      if (ticket.TicketType.isRemote || ticket.TicketType.name.includes('Sem hotel')) {
+        setTicketType(modalityTicket.ONLINE_MODALITY);
+        setUnauthorizedMessage(unauthorizedMsg.ONLINE_MODALITY_MSG);
+      }
+    }
+  }, [ticket]);
+
   return (
     <HotelsSections>
       <TitleSection>Escolha de Hotel e quarto</TitleSection>
-      <DisplaySection title={'Primeiro, escolha seu hotel'}>
-        {hotels ? hotels.map((data, index) => (
-          <CardContainer key={index} title={data.name} image={data.image} />
-        )) : ' '}
-      </DisplaySection>
+      {paidTicket && (
+        <>
+          {ticketType === modalityTicket.AT_EVENT_MODALITY && <HotelsContainer hotels={hotels} />}
+          {ticketType === modalityTicket.ONLINE_MODALITY && (
+            <UnauthorizedScreen>{unauthorizedMessage}</UnauthorizedScreen>
+          )}
+        </>
+      )}
+      {!paidTicket && <UnauthorizedScreen>{unauthorizedMessage}</UnauthorizedScreen>}
     </HotelsSections>
   );
 }
+
+function HotelsContainer({ hotels }) {
+  return (
+    <DisplaySection title={'Primeiro, escolha seu hotel'}>
+      {hotels && hotels.map((data, index) => <CardContainer key={index} title={data.name} image={data.image} />)}
+    </DisplaySection>
+  );
+}
+
 const HotelsSections = styled.div`
+  height: 100%;
+  width: 100%;
   display: flex;
   flex-direction: column;
   row-gap: 32px;
@@ -27,4 +71,3 @@ const HotelsSections = styled.div`
 const TitleSection = styled.p`
   font-size: 28px;
 `;
-
