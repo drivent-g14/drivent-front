e7b582cd98fa610429828d4e36014d20f143eb78
Matheus <matheusnavesamorim@gmail.com>
2023-01-03 22:55:51 -0300

    feat: change screen

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,3 +1,16 @@
+import { useEffect, useState } from 'react';
+import { useGetTicket } from '../../../hooks/api/useTicket';
+
 export default function Activities() {
+  const { ticket } = useGetTicket();
+  const [activities, setActivities] = useState();
+
+  useEffect(() => {
+    if (ticket) {
+      setActivities(true);
+      console.log(ticket.status);
+    }
+  }, [ticket]);
+  
   return 'Atividades: Em breve!';
 }
