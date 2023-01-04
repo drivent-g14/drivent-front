673ad35c60210dc1a9644e7d895011de74cdc75b
Matheus <matheusnavesamorim@gmail.com>
2023-01-04 17:09:11 -0300

    fix: show activities

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -6,15 +6,19 @@ import styled from 'styled-components';
 export default function Activities() {
   const { ticket } = useGetTicket();
   const [activities, setActivities] = useState(false);
+  const [message, setMessage] = useState('Você precisa ter confirmado pagamento antes de fazer a escolha de atividades');
   
   useEffect(() => {
-    if (ticket && ticket.status === 'PAID') setActivities(true);
+    if (ticket && ticket.status === 'PAID' && ticket.TicketType.isRemote) 
+      setMessage('Sua modalidade de ingresso não necessita escolher atividade. Você terá acesso a todas as atividades.');
+    else if(ticket && ticket.status === 'PAID')
+      setActivities(true);
   }, [ticket]);
   
   return (
     <PaymentContainer>
       <TitleSection>Escolha de atividades</TitleSection>
-      {activities ? 'Teste' : <UnauthorizedScreen>Você precisa ter confirmado pagamento antes de fazer a escolha de atividades</UnauthorizedScreen>}
+      {activities ? 'Teste' : <UnauthorizedScreen>{message}</UnauthorizedScreen>}
     </PaymentContainer>
   );
 }
