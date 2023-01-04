bb8c6309a695a9fc6b2672bee2950f7bd5bba9e8
Matheus <matheusnavesamorim@gmail.com>
2023-01-03 23:04:59 -0300

    feat: screen no payments

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -1,16 +1,32 @@
 import { useEffect, useState } from 'react';
+import UnauthorizedScreen from '../../../components/Dashboard/Errors/UnauthorizedScreen';
 import { useGetTicket } from '../../../hooks/api/useTicket';
+import styled from 'styled-components';
 
 export default function Activities() {
   const { ticket } = useGetTicket();
-  const [activities, setActivities] = useState();
-
+  const [activities, setActivities] = useState(false);
+  
   useEffect(() => {
-    if (ticket) {
-      setActivities(true);
-      console.log(ticket.status);
-    }
+    if (ticket && ticket.status === 'PAID') setActivities(true);
   }, [ticket]);
   
-  return 'Atividades: Em breve!';
+  return (
+    <PaymentContainer>
+      <TitleSection>Escolha de atividades</TitleSection>
+      {activities ? 'Teste' : <UnauthorizedScreen>Você precisa ter confirmado pagamento antes de fazer a escolha de atividades</UnauthorizedScreen>}
+    </PaymentContainer>
+  );
 }
+
+const PaymentContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  row-gap: 32px;
+`;
+
+const TitleSection = styled.p`
+  font-size: 28px;
+`;
