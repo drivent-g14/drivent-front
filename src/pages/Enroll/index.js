fbcae59b5cb168a2ec153fc93b3811eaaf43486d
carlos_casanova <carlosctu94@gmail.com>
2023-01-07 16:10:34 -0300

    feat: adding github auth

--- src/pages/Enroll/index.js
+++ src/pages/Enroll/index.js
@@ -10,8 +10,10 @@ import { Row, Title, Label } from '../../components/Auth';
 import Link from '../../components/Link';
 
 import EventInfoContext from '../../contexts/EventInfoContext';
-
+import githubLogo from '../../assets/images/github-icon-9.png';
 import useSignUp from '../../hooks/api/useSignUp';
+import styled from 'styled-components';
+import useOAuthGithub from '../../hooks/api/useOAuth';
 
 export default function Enroll() {
   const [email, setEmail] = useState('');
@@ -21,8 +23,9 @@ export default function Enroll() {
   const { loadingSignUp, signUp } = useSignUp();
 
   const navigate = useNavigate();
-  
+
   const { eventInfo } = useContext(EventInfoContext);
+  const { oAuthGithub } = useOAuthGithub();
 
   async function submit(event) {
     event.preventDefault();
@@ -40,6 +43,30 @@ export default function Enroll() {
     }
   }
 
+  function redirectToGithub() {
+    const GITHUB_URL = 'https://github.com/login/oauth/authorize';
+    const CLIENT_ID = '663f23177c565f1cbd23';
+    const authURL = `${GITHUB_URL}?client_id=${CLIENT_ID}`;
+    window.location.href = authURL;
+  }
+
+  window.onload = async() => {
+    const queryParamsString = window.location.search.substring(1);
+    const queryParams = queryParamsString.split('&').reduce((acc, curr) => {
+      const [key, value] = curr.split('=');
+      acc[key] = value;
+      return acc;
+    }, {});
+    if (queryParams.code) {
+      try {
+        const userInfo = await oAuthGithub(queryParams.code);
+        await signUp(`${userInfo.login}@github.com`, userInfo.node_id);
+        toast('Usuário cadastrado com sucesso!');
+        navigate('/sign-in');
+      } catch (error) {}
+    }
+  };
+
   return (
     <AuthLayout background={eventInfo.backgroundImageUrl}>
       <Row>
@@ -49,15 +76,58 @@ export default function Enroll() {
       <Row>
         <Label>Inscrição</Label>
         <form onSubmit={submit}>
-          <Input label="E-mail" type="text" fullWidth value={email} onChange={e => setEmail(e.target.value)} />
-          <Input label="Senha" type="password" fullWidth value={password} onChange={e => setPassword(e.target.value)} />
-          <Input label="Repita sua senha" type="password" fullWidth value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} />
-          <Button type="submit" color="primary" fullWidth disabled={loadingSignUp}>Inscrever</Button>
+          <Input label="E-mail" type="text" fullWidth value={email} onChange={(e) => setEmail(e.target.value)} />
+          <Input
+            label="Senha"
+            type="password"
+            fullWidth
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Input
+            label="Repita sua senha"
+            type="password"
+            fullWidth
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <Button type="submit" color="primary" fullWidth disabled={loadingSignUp}>
+            Inscrever
+          </Button>
         </form>
       </Row>
       <Row>
         <Link to="/sign-in">Já está inscrito? Faça login</Link>
+        <GithubButton onClick={() => redirectToGithub()}>
+          <GithubLogo src={githubLogo} alt="github-sign-up" />
+          <VerticalDivider></VerticalDivider>
+          Sign-up com Github
+        </GithubButton>
       </Row>
     </AuthLayout>
   );
 }
+
+const GithubLogo = styled.img`
+  height: 100%;
+`;
+
+const VerticalDivider = styled.div`
+  width: 1px;
+  height: 70%;
+  background-color: white;
+  margin: 0 5px 0 0;
+`;
+
+const GithubButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+  background-color: #444444;
+  color: white;
+  padding-right: 5px;
+  border-radius: 4px;
+  cursor: pointer;
+  margin-top: 10px;
+`;
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,6 +1,6 @@
 import { useState, useContext } from 'react';
 import { toast } from 'react-toastify';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import AuthLayout from '../../layouts/Auth';
 
@@ -14,13 +14,15 @@ import UserContext from '../../contexts/UserContext';
 
 import useSignIn from '../../hooks/api/useSignIn';
 import useOAuthGithub from '../../hooks/api/useOAuth';
+import styled from 'styled-components';
+import githubLogo from '../../assets/images/github-icon-9.png';
 
 export default function SignIn() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const { loadingSignIn, signIn } = useSignIn();
-  const { github, oAuthGithub, oAuthGithubError } = useOAuthGithub();
+  const {  oAuthGithub } = useOAuthGithub();
   const { eventInfo } = useContext(EventInfoContext);
   const { setUserData } = useContext(UserContext);
 
@@ -32,7 +34,7 @@ export default function SignIn() {
       const userData = await signIn(email, password);
       setUserData(userData);
       toast('Login realizado com sucesso!');
-      //navigate('/dashboard');
+      navigate('/dashboard');
     } catch (err) {
       toast('Não foi possível fazer o login!');
     }
@@ -46,12 +48,16 @@ export default function SignIn() {
       return acc;
     }, {});
     if (queryParams.code) {
-      console.log('iuuu');
-      console.log(queryParams.code);
       try {
-        await oAuthGithub(queryParams.code);
+        const userInfo = await oAuthGithub(queryParams.code);
+        const userData = await signIn(`${userInfo.login}@github.com`, userInfo.node_id);
+        setUserData(userData);
+        toast('Login realizado com sucesso!');
+        navigate('/dashboard');
       } catch (error) {
         console.log(error);
+        toast('Usuário não encontrado!');
+        navigate('/enroll');
       }
     }
   };
@@ -85,9 +91,36 @@ export default function SignIn() {
           </Button>
         </form>
       </Row>
-      <button onClick={() => redirectToGithub()}>Não possui login? Inscreva-se</button>
-      {/* <Link to="/enroll">Não possui login? Inscreva-se</Link> */}
+      <Link to="/enroll">Não possui login? Inscreva-se</Link>
+      <GithubButton onClick={() => redirectToGithub()}>
+        <GithubLogo src={githubLogo} alt="github-login" />
+        <VerticalDivider></VerticalDivider>
+        Login com Github
+      </GithubButton>
       <Row></Row>
     </AuthLayout>
   );
 }
+
+const GithubLogo = styled.img`
+  height: 100%;
+`;
+
+const VerticalDivider = styled.div`
+  width: 1px;
+  height: 70%;
+  background-color: white;
+  margin: 0 5px 0 0;
+`;
+
+const GithubButton = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 30px;
+  background-color: #444444;
+  color: white;
+  padding-right: 5px;
+  border-radius: 4px;
+  cursor: pointer;
+`;
